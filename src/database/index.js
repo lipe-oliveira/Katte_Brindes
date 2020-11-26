02f5fc9f97381444e6558593e1d0989f40e0100e
Felipe <felipeoliveira.b@gmail.com>
2020-11-26 02:52:18 -0300

    msg

--- src/database/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb+srv://behl:homemaranha@cluster.2kqq4.mongodb.net/Katte_Brindes?retryWrites=true&w=majority');
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
