df1b491710f576668bbe15676d34aa9aca90b0ca
Felipe <felipeoliveira.b@gmail.com>
2020-12-10 22:24:29 -0300

    auto

--- src/app/controllers/controller_sobre.js
+++ src/app/controllers/controller_sobre.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Sobre = require('../models/sobre')
 
 router.get('/get_logo', async(req, res) => {
-    const spbre= await Sobre.findOne({});
+    const sobre= await Sobre.findOne({});
     return( 
         res.send(sobre)
     )
