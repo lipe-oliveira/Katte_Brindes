ac36a4548bdf7c230c81e547f72a4daaffd41877
Felipe <felipeoliveira.b@gmail.com>
2020-12-10 22:21:27 -0300

    auto

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:true, limit:'50mb'}))
 app.use(cors());
 require("./app/controllers/controller")(app);
+require("./app/controllers/controller_sobre")(app);
 
 app.listen(process.env.PORT || 3000);
